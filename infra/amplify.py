2b57a0dab547c191add202f234cc6949813d5667
Advaith H L <niceadvaith@gmail.com>
2025-04-15 11:47:39 +0200

    Add Amplify app

---
+++ infra/amplify.py
@@ -0,0 +1,16 @@
+import pulumi_aws as aws
+
+example = aws.amplify.App("careportal_frontend",
+    name="checkmate-frontend-prod",
+    custom_rules=[{
+        "source": "/<*>",
+        "status": "404-200",
+        "target": "/index.html",
+    }])
+
+example_prod_branch = aws.amplify.Branch("careportal_frontend_prod_branch",
+    app_id=example.id,
+    branch_name="prod",
+    stage="PRODUCTION",
+    description="CarePortalBeta frontend production"
+)
